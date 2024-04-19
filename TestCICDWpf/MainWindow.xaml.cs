d5ba6df0be843939908b3a5605bf988f358cf547
flaxmines <96043718+flaxmines@users.noreply.github.com>
2024-04-19 17:28:21 +0300

    upd

--- TestCICDWpf/MainWindow.xaml.cs
+++ TestCICDWpf/MainWindow.xaml.cs
@@ -1,24 +1,31 @@
-﻿using System.Text;
+﻿using AutoUpdaterDotNET;
+using System.IO;
+using System.Net;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace TestCICDWpf
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+            AutoUpdater.InstalledVersion = new Version("1.1");
+
+            DispatcherTimer timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(10) };
+            timer.Tick += delegate
+            {
+                AutoUpdater.Start("ftp:eu-west-1.sftpcloud.io/UpdateInfo.xml", new NetworkCredential("a4fb7a14b0d14773b0b68c41434ec015", "riVgMWA3Y93J1fV2Vw8NlnZmCPYJgmzj"));
+
+                var currentDirectory = new DirectoryInfo(@"C:\Users\Flax\Documents\GitHub\TestCICD\TestCICDWpf");
+                if (currentDirectory.Parent != null)
+                {
+                    AutoUpdater.InstallationPath = currentDirectory.Parent.FullName;
+                }
+            };
+            timer.Start();
         }
     }
 }
