bd433b1191a7cd0b671ef9ff4404c28359251f07
flaxmines <96043718+flaxmines@users.noreply.github.com>
2024-04-20 13:42:49 +0300

    added ftp deploy

--- TestCICDWpf/MainWindow.xaml.cs
+++ TestCICDWpf/MainWindow.xaml.cs
@@ -12,12 +12,13 @@ namespace TestCICDWpf
         {
             InitializeComponent();
 
+            AutoUpdater.FtpCredentials = new NetworkCredential("a4fb7a14b0d14773b0b68c41434ec015", "riVgMWA3Y93J1fV2Vw8NlnZmCPYJgmzj");
             AutoUpdater.InstalledVersion = new Version("1.1");
 
             DispatcherTimer timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(10) };
             timer.Tick += delegate
             {
-                AutoUpdater.Start("ftp:eu-west-1.sftpcloud.io/UpdateInfo.xml", new NetworkCredential("a4fb7a14b0d14773b0b68c41434ec015", "riVgMWA3Y93J1fV2Vw8NlnZmCPYJgmzj"));
+                AutoUpdater.Start("ftp://eu-west-1.sftpcloud.io/UpdateInfo.xml", new NetworkCredential("a4fb7a14b0d14773b0b68c41434ec015", "riVgMWA3Y93J1fV2Vw8NlnZmCPYJgmzj"));
 
                 var currentDirectory = new DirectoryInfo(@"C:\Users\Flax\Documents\GitHub\TestCICD\TestCICDWpf");
                 if (currentDirectory.Parent != null)
