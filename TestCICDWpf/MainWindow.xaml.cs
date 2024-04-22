80c31986cc54b19074aa023ffd4923eb77db1bc6
flaxmines <96043718+flaxmines@users.noreply.github.com>
2024-04-22 15:58:58 +0300

    upd

--- TestCICDWpf/MainWindow.xaml.cs
+++ TestCICDWpf/MainWindow.xaml.cs
@@ -12,8 +12,6 @@ namespace TestCICDWpf
         {
             InitializeComponent();
 
-            //var fgfg = AppDomain.CurrentDomain.BaseDirectory;
-
             AutoUpdater.FtpCredentials = new NetworkCredential("a4fb7a14b0d14773b0b68c41434ec015", "riVgMWA3Y93J1fV2Vw8NlnZmCPYJgmzj");
             AutoUpdater.InstalledVersion = new Version("1.1");
 
@@ -22,12 +20,11 @@ namespace TestCICDWpf
             {
                 AutoUpdater.Start("ftp://eu-west-1.sftpcloud.io/UpdateInfo.xml", new NetworkCredential("a4fb7a14b0d14773b0b68c41434ec015", "riVgMWA3Y93J1fV2Vw8NlnZmCPYJgmzj"));
 
-
-                //   var currentDirectory = new DirectoryInfo(@"C:\Users\Flax\Documents\GitHub\TestCICD\TestCICDWpf");
-                //if (currentDirectory.Parent != null)
-                //{
-                //    AutoUpdater.InstallationPath = currentDirectory.Parent.FullName;
-                //}
+                var currentDirectory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+                if (currentDirectory.Parent != null)
+                {
+                    AutoUpdater.InstallationPath = currentDirectory.Parent.FullName;
+                }
             };
             timer.Start();
         }
