fdccbc3a35513d366f4246fb77a567f71b437d5a
flaxmines <96043718+flaxmines@users.noreply.github.com>
2024-04-24 15:43:03 +0300

    upd

--- TestCICDWpf/MainWindow.xaml.cs
+++ TestCICDWpf/MainWindow.xaml.cs
@@ -1,8 +1,4 @@
-﻿using AutoUpdaterDotNET;
-using System.IO;
-using System.Net;
-using System.Windows;
-using System.Windows.Threading;
+﻿using System.Windows;
 
 namespace TestCICDWpf
 {
@@ -11,22 +7,6 @@ namespace TestCICDWpf
         public MainWindow()
         {
             InitializeComponent();
-
-            AutoUpdater.FtpCredentials = new NetworkCredential("a4fb7a14b0d14773b0b68c41434ec015", "riVgMWA3Y93J1fV2Vw8NlnZmCPYJgmzj");
-            AutoUpdater.InstalledVersion = new Version("1.1");
-
-            DispatcherTimer timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(10) };
-            timer.Tick += delegate
-            {
-                AutoUpdater.Start("ftp://eu-west-1.sftpcloud.io/UpdateInfo.xml", new NetworkCredential("a4fb7a14b0d14773b0b68c41434ec015", "riVgMWA3Y93J1fV2Vw8NlnZmCPYJgmzj"));
-
-                var currentDirectory = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
-                if (currentDirectory.Parent != null)
-                {
-                    AutoUpdater.InstallationPath = AppDomain.CurrentDomain.BaseDirectory;
-                }
-            };
-            timer.Start();
         }
     }
 }
