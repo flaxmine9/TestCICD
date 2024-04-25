a7aef10112ad4017bff5332090c9f407fce42a80
flaxmines <96043718+flaxmines@users.noreply.github.com>
2024-04-25 11:41:46 +0300

    upd

---
+++ AutoUpdater/AutoUpdateWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Windows;
+
+namespace AutoUpdater
+{
+    public partial class AutoUpdateWindow : Window
+    {
+        public AutoUpdateWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void No_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Yes_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
--- TestCICDWpf/MainWindow.xaml.cs
+++ TestCICDWpf/MainWindow.xaml.cs
@@ -1,12 +1,73 @@
-﻿using System.Windows;
+﻿using FluentFTP;
+using System.Diagnostics;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace TestCICDWpf
 {
     public partial class MainWindow : Window
     {
+        private string Url { get; set; } = "eu-central-1.sftpcloud.io";
+        private string UserName { get; set; } = "ea066c0c2dde4e91b2d8b8b881df1304";
+        private string PassWord { get; set; } = "JpADT8knYnuULHwDfSdTUd8oioPGF25E";
+
+        private FtpClient _client;
+
+        private bool _isOpen = false;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            _client = new FtpClient(Url, UserName, PassWord);
+            _client.AutoConnect();
+
+            DispatcherTimer timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(5) };
+            timer.Tick += delegate
+            {
+                if (Process.GetProcessesByName("AutoUpdater").Any())
+                    return;
+
+                Version newVersion = CheckNewVersion("Project/Version/test.txt");
+
+                var versionApp = Assembly.GetEntryAssembly()!.GetName().Version!;
+
+                //string currentVersion = versionApp.Major + "." + versionApp.Minor + "." + versionApp.Build;
+
+                if (newVersion != versionApp)
+                {
+                    var msg = MessageBox.Show("Новое обновление", "Обновление", MessageBoxButton.YesNo);
+                    if (msg == MessageBoxResult.Yes)
+                    {
+                        var currentProcess = Process.GetCurrentProcess();
+
+                        // Запускаем процесс автоапдейтера (.exe)
+                        Process.Start(currentProcess.MainModule!.FileName!.Replace(currentProcess.MainModule.ModuleName!, "AutoUpdater.exe"));
+
+                        currentProcess.CloseMainWindow();
+                    }
+                }
+            };
+            timer.Start();
+        }
+
+        public Version CheckNewVersion(string remotePath)
+        {
+            var boolDownload = _client.DownloadBytes(out var bytes, remotePath: remotePath);
+            if (boolDownload)
+            {
+                var serverVersion = Encoding.UTF8.GetString(bytes).Split(".sync-state-v").Last();
+
+                var splitted = serverVersion.Split('.');
+                if (splitted.Any())
+                {
+                    return new Version(Convert.ToInt32(splitted[0]), Convert.ToInt32(splitted[1]), Convert.ToInt32(splitted[2]), 0);
+                }
+            }
+
+            return new Version();
         }
     }
 }
